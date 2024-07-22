dfd1bbf9de926ee5c79dfe4e62b00d580e4d8079
danielnifty <danielobichere@gmail.com>
2024-07-22 17:13:44 +0100

    about us page created

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,6 +1,210 @@
 <template>
-      <!-- our success -->
+  <!-- our success -->
   <div class="grid md:grid-cols-2 gap-2 pt-20 md:pl-20 pl-5 md:pr-20 pr-2 pb-0">
+    <div class="md:p-10 p-5 rounded-xl">
+      <img src="/img/about.jpg" class="rounded-xl" />
+    </div>
+
+    <div class="grid grid-cols-1 gap-1 md:p-10 p-2 md:space-y-10 space-y-5">
+      <div class="space-y-1">
+        <h4 class="text-[15px] font-bold text-[#7e7d7d]">About us</h4>
+        <h2 class="font-bold md:text-4xl text-2xl text-[#3f3a64]">
+          We make the best strategies for you, Enhancing your success
+        </h2>
+      </div>
+
+      <div class="text-[#7e7d7d] font-bold text-[15px]">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud ullamco laboris nisi ut ex ea.
+        </p>
+      </div>
+
+      <div class="space-y-6 text-sm md:text-xl">
+        <div class="flex space-x-2">
+          <span
+            class="fa fa-check bg-blue-900 p-1 rounded-full text-white"
+          ></span>
+          <p class="font-bold text-[#3f3a64]">UI/UX Design and Development</p>
+        </div>
+
+        <div class="flex space-x-2">
+          <span
+            class="fa fa-check bg-blue-900 p-1 rounded-full text-white"
+          ></span>
+          <p class="font-bold text-[#3f3a64]">UI/UX Design and Development</p>
+        </div>
+
+        <div class="flex space-x-2">
+          <span
+            class="fa fa-check bg-blue-900 p-1 rounded-full text-white"
+          ></span>
+          <p class="font-bold text-[#3f3a64]">UI/UX Design and Development</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- our mission  -->
+  <div
+    class="grid md:grid-cols-3 place-items-center gap-10 pt-20 md:pl-20 pl-5 md:pr-20 pr-2 pb-10"
+  >
+    <div
+      class="grid md:grid-cols-1 pt-5 rounded-xl shadow-xl hover:shadow-blue-500/50 text-[#3f3a64]"
+    >
+      <div class="flex justify-center space-x-5 pb-5 place-items-center p-5">
+        <span
+          class="grid md:grid-cols-1 fa fa-book font-bold text-2xl rounded-full w-20 h-20 pt-5 text-center hover:text-white hover:bg-[#3f3a64] place-items-center"
+          ><i class="text-center"></i
+        ></span>
+        <div class="p">
+          <h1 class="font-bold text-[#7e7d7d] text-xl">our</h1>
+          <h1 class="font-bold text-2xl text-[#3f3a64]">MISSION</h1>
+        </div>
+      </div>
+
+      <div class="pl-2 pr-2 text-center pb-10">
+        <h2 class="text-[#7e7d7d] text-xl">
+          If you are looking for high-quality and reliable online courses.
+        </h2>
+      </div>
+    </div>
+
+    <div
+      class="grid md:grid-cols-1 pt-5 rounded-xl shadow-xl hover:shadow-blue-500/50 text-[#3f3a64]"
+    >
+      <div class="flex justify-center space-x-5 pb-5 place-items-center p-5">
+        <span
+          class="grid md:grid-cols-1 fa fa-book font-bold text-2xl rounded-full w-20 h-20 pt-5 text-center hover:text-white hover:bg-[#3f3a64] place-items-center"
+          ><i class="text-center"></i
+        ></span>
+        <div class="p">
+          <h1 class="font-bold text-[#7e7d7d] text-xl">our</h1>
+          <h1 class="font-bold text-2xl text-[#3f3a64]">VISION</h1>
+        </div>
+      </div>
+
+      <div class="pl-2 pr-2 text-center pb-10">
+        <h2 class="text-[#7e7d7d] text-xl">
+          If you are looking for high-quality and reliable online courses.
+        </h2>
+      </div>
+    </div>
+
+    <div
+      class="grid md:grid-cols-1 pt-5 rounded-xl shadow-xl hover:shadow-blue-500/50 text-[#3f3a64]"
+    >
+      <div class="flex justify-center space-x-5 pb-5 place-items-center p-5">
+        <span
+          class="grid md:grid-cols-1 fa fa-book font-bold text-2xl rounded-full w-20 h-20 pt-5 text-center hover:text-white hover:bg-[#3f3a64] place-items-center"
+          ><i class="text-center"></i
+        ></span>
+        <div class="p">
+          <h1 class="font-bold text-[#7e7d7d] text-xl">our</h1>
+          <h1 class="font-bold text-2xl text-[#3f3a64]">GOAL</h1>
+        </div>
+      </div>
+
+      <div class="pl-2 pr-2 text-center pb-10">
+        <h2 class="text-[#7e7d7d] text-xl">
+          If you are looking for high-quality and reliable online courses.
+        </h2>
+      </div>
+    </div>
+  </div>
+
+  <!-- our success -->
+  <div class="grid md:grid-cols-2 gap-2 pt-20 md:pl-20 pl-5 md:pr-20 pr-2 pb-0">
+    <div class="grid grid-cols-1 gap-1 md:p-10 p-5 space-y-10">
+      <div class="space-y-1">
+        <h4 class="md:text-[15px] font-bold text-[#7e7d7d]">
+          YOUR SUCCESS IS OUR SUCCESS
+        </h4>
+        <h2 class="font-bold md:text-4xl text-2xl text-[#3f3a64]">
+          Web design, marketing & SEO solutions that get results
+        </h2>
+      </div>
+
+      <section class="w3l-progress" id="progress">
+        <div class="midd-w3">
+          <div class="container py- lg-5 py-md-4 py-2">
+            <div class="row align-items-center">
+              <div class="">
+                <div class="progress-info info1">
+                  <h6 class="progress-tittle">
+                    Marketing <span class="">80%</span>
+                  </h6>
+                  <div class="progress">
+                    <div
+                      class="progress-bar progress-bar-striped gradient-1"
+                      role="progressbar"
+                      style="width: 80%"
+                      aria-valuenow="90"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </div>
+                <div class="progress-info info2">
+                  <h6 class="progress-tittle">
+                    Responsive Web design <span class="">95%</span>
+                  </h6>
+                  <div class="progress">
+                    <div
+                      class="progress-bar progress-bar-striped gradient-2"
+                      role="progressbar"
+                      style="width: 95%"
+                      aria-valuenow="95"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </div>
+                <div class="progress-info info3">
+                  <h6 class="progress-tittle">
+                    Analytics <span class="">60%</span>
+                  </h6>
+                  <div class="progress">
+                    <div
+                      class="progress-bar progress-bar-striped gradient-3"
+                      role="progressbar"
+                      style="width: 60%"
+                      aria-valuenow="95"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </div>
+                <div class="progress-info info4 mb-0">
+                  <h6 class="progress-tittle">
+                    Web Development <span class="">85%</span>
+                  </h6>
+                  <div class="progress">
+                    <div
+                      class="progress-bar progress-bar-striped gradient-4"
+                      role="progressbar"
+                      style="width: 85%"
+                      aria-valuenow="95"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <div class="md:p-10 p-5 rounded-xl">
+      <img src="/img/about.jpg" class="rounded-xl" />
+    </div>
+  </div>
+
+ <!-- our success -->
+ <div class="grid md:grid-cols-2 gap-2 pt-20 md:pl-20 pl-5 md:pr-20 pr-2 pb-0">
     <div class="md:p-10 p-5 rounded-xl">
       <img src="/img/about.jpg" class="rounded-xl" />
     </div>
