856ccc9635c2882885f2f07ed947e77994166b44
Danielnifty1 <danielobichere@gmail.com>
2024-08-28 16:40:29 +0100

    fix font awesome icons

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -31,7 +31,7 @@
 
     </div>
 
-    <div class="grid grid-cols-1 gap-1 md:p-10 p-2 md:space-y-10 space-y-5">
+    <div class="grid grid-cols-1 gap-1 md:p-10 p-2 md:space-y-1 space-y-5 place-items-start">
       <div class="space-y-1">
         <h4 class="text-[15px] font-bold text-[#7e7d7d]">About CEO</h4>
         <h2 class="font-bold  text-2xl text-[#3f3a64]">
