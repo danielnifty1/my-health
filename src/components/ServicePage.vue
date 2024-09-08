0cf569adef743506580f648aa48adbd7cef61618
danielnifty1 <90337586+danielnifty1@users.noreply.github.com>
2024-09-08 11:03:31 +0100

    Update ServicePage.vue merge

--- src/components/ServicePage.vue
+++ src/components/ServicePage.vue
@@ -1,188 +1,192 @@
 <template>
-    <div class="grid grod-cols-1 place-items-center mt-10 space-y-5 mb-10">
-      <p class="font-bold text-blue-800 text-2xl">Focus on Your Business</p>
-      <p class="text-2xl font-bold text-center text-semibold">
-        A STANDAERD REGISTERED HEALTH <br />CONSULTANCY
-      </p>
-      <p class="text-center w-6/12">
-        Trusted Partner in Public Health, Leadership & Ethical Development
-      </p>
-  
-      <div class="flex space-x-3 pl0">
-        <a href="/contact" class="bg-blue-800 text-white p-2 rounded">
-          Get Started
-        </a>
-      </div>
+  <div class="grid grod-cols-1 place-items-center mt-10 space-y-5 mb-10">
+    <p class="font-bold text-blue-800 text-2xl">Focus on Your Business</p>
+    <p class="text-2xl font-bold text-center text-semibold">
+      A STANDAERD REGISTERED HEALTH <br />CONSULTANCY
+    </p>
+    <p class="text-center w-6/12">
+      Trusted Partner in Public Health, Leadership & Ethical Development
+    </p>
+
+    <div class="flex space-x-3 pl0">
+      <a href="/contact" class="bg-blue-800 text-white p-2 rounded">
+        Get Started
+      </a>
     </div>
-  
-    <div class="bg-[#f8f8f8] grid grid-cols-1 md:pl-40 pl-5 pr-5 md:pr-40 mb-10 pt-10">
-      <div class="grid md:grid-cols-2 place-items-start space-x-5 P-24">
-        <div class="grid grid-cols-1 space-y-5">
-          <p class="font-bold text-blue-800 text-xl">What We Do</p>
-          <p class="text-4xl font-bold text-semibold">Our Services</p>
-          <p class="  ">
-            Operational research and consultancy services in the prevention and
-            control of public health important endemic diseases, training and
-            mobilization of MDAs and disease endemic communities in the prevention
-            and control of endemic diseases as well as creation of emerging true,
-            competent, inspirational and transformational leaders with strength of
-            character of honesty, integrity, industry, teamwork, dominant
-            personality, and emotional intelligence. Also, Care Consultancy is
-            involved in training and mentoring students (both undergraduate and
-            postgraduate) on thematic academic areas like seminar writing and
-            presentation, Dissertation and Thesis organization, reporting and
-            preparation for internal/external examination as well as guide the
-            students on research grant proposal writing and funding sources.
-          </p>
-        </div>
-  
-        <div class="mt-5 md:mt-0 hidden md:grid">
-          <img src="/img/img.jpg" class="rounded-xl hover:-translate-y-1 hover:scale-110 duration-300" />
-        </div>
-      </div>
-  
-      <div class="grid md:grid-cols-2 place-items-start md:space-x-10 md:P-24 mt-24 pb-10">
-        <div class="mb-5 md:mt-0">
-          <img src="/img/img2.jpg" class="rounded-xl hover:-translate-y-1 hover:scale-110 duration-300" />
-        </div>
-        <div class="grid grid-cols-1 space-y-5">
-          <p class="font-bold text-blue-800 text-xl">What We Do</p>
-          <p class="text-4xl font-bold text-semibold">Our Background</p>
-          <p class="  ">
-            Prof. Bertram Ekejiuba Bright Nwoke is from Umuariam, Obowo LGA in Imo
-            State, Nigeria. He was a pioneer student of Institute of Management
-            and Technology, Enugu where he obtained OND (Science Laboratory
-            Technology) in 1975. He was one of the pioneer students of University
-            of Jos who matriculated in 1976. He obtained B.Sc. (Zoology) Second
-            Class Upper Division in 1979 and came out as the first best graduating
-            student of Zoology, University of Jos. He also has to his credit an
-            MSc degree in Applied Entomology & Parasitology (1981) and PhD degree
-            in Medical Parasitology & Entomology (1987) all from the University of
-            Jos. He is well trained in Total Quality Management and has a
-            certificate on it.
-          </p>
-        </div>
-      </div>
-    </div>
-  
-    <div
-      class="grid md:grid-cols-2 md:pl-40 md:pr-40 pr-5 pl-5  mb-10 pt-10 space-x-10 place-items-start"
-    >
-      <div class="grid space-y-5">
-        <p class="text-center md:text-start">CONTRIBUTIONS TO THE GROWTH AND DEVELOPMENT OF SCIENCE</p>
-        <p class="text-2xl font-bold text-semibold text-center">SERVICES TO SCIENCE</p>
-        <p>
-          Prof. B. E. B. Nwoke has strongly contributed to academic leadership and growth as well as stimulated superior and excellent research studies and learning in the University. He has been involved in external examination of postgraduate students (PhD and or Masters Degree) in 12 Universities and in the assessment of more than 50 colleagues to the ranks of Professor/Reader in Nigeria and abroad. He has also successfully supervised more than 300 undergraduate projects, at least 67 Masters Degree and at least 75 PhD Theses. Out these postgraduates, 22 of them are Professors. One of his postgraduate students is now a Vice Chancellor and another one is currently a Deputy Vice Chancellor. He has to his credit not less than 213 academic and professional publications (journal, proceedings, technical reports, book, and book chapter). Prof Nwoke has been involved in the postgraduate external examination of the following Institutions
+  </div>
+
+  <div class="bg-[#f8f8f8] grid grid-cols-1 md:pl-40 pl-5 pr-5 md:pr-40 mb-10 pt-10">
+    <div class="grid md:grid-cols-2 place-items-start space-x-5 P-24">
+      <div class="grid grid-cols-1 space-y-5">
+        <p class="font-bold text-blue-800 text-xl">What We Do</p>
+        <p class="text-4xl font-bold text-semibold">Our Services</p>
+        <p class="  ">
+          Operational research and consultancy services in the prevention and
+          control of public health important endemic diseases, training and
+          mobilization of MDAs and disease endemic communities in the prevention
+          and control of endemic diseases as well as creation of emerging true,
+          competent, inspirational and transformational leaders with strength of
+          character of honesty, integrity, industry, teamwork, dominant
+          personality, and emotional intelligence. Also, Care Consultancy is
+          involved in training and mentoring students (both undergraduate and
+          postgraduate) on thematic academic areas like seminar writing and
+          presentation, Dissertation and Thesis organization, reporting and
+          preparation for internal/external examination as well as guide the
+          students on research grant proposal writing and funding sources.
         </p>
-  
-        <div class="space-y-5">
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Federal University of Technology Owerri, (MSc & PhD)<br />
-          </p>
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> University of Nigeria, Nsukka (MSc & PhD)<br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> University of Benin (Doctor of Optometry, OD)
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> University of Jos (PhD)<br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> University of Calabar (PhD)
-            <br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> University of Port Harcourt (PhD)<br />
-          </p>
-  
-          <!-- ==== -->
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> University of Abuja (PhD)<br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Nnamdi Azikiwe University, Awka (MSc & PhD)
-          </p>
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Ambrose Ali University, Ekpoma (PhD)<br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Delta State University, Abraka (MSc)<br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Michael Okpara University of Agriculture, Umuahia (MSc & PhD)<br />
-          </p>
-        </div>
       </div>
-  
-      <div>
-        <div class="md:space-y-5 space-y-2 mt-5 md:mt-0">
-          <p class="text-center">SERVICE TO HUMANITY</p>
-          <p class="text-sm md:text-2xl font-bold text-semibold text-center">CONTRIBUTIONS TO GOVERNMENT AND COMMUNITY SERVICE</p>
-  
-         
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> 	Pioneer Board Member, Imo State University Teaching Hospital Board, 2003-2007<br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Chairman, Imo State Technical Committee on the Review of Curriculum and Restructuring of Michael Okpara College of Agriculture [MOCA] Umuagwo, 2004. This led to the establishment of Michael Okpara Polytechnic Umuagwo
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Chairman, Imo State Government Board of Inquiry into the Health Management Board, 2005 <br />
-          </p>
-  
-  
-  
-          <!-- ================================== -->
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Member, Imo State Government 2007 Transition Committee<br />
-          </p>
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Chairman, Sub-Committee on Land and Environment, Imo State Government 2007 Transition Committee<br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span>  Chairman, Imo State Government Board of Inquiry into the Health Management Board, 2005  
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span>  Chairman, Sub-Committee on Land and Environment, Imo State Government 2007 Transition Committee<br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> 	Chairman, Ministerial Committee of the Review of the Industrial Training Fund (ITF) System Audit, June-July, 2008.
-            <br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Steering Committee Member, Imo State Vision 20: 2020/>
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Chairman, Imo State University Vision 20:2020<br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span>  Chairman, Imo State Government Special Committee on the Return of Secondary Schools to Voluntary Agencies and Missionaries, 2009.<br />
-          </p>
-  
-          <p>
-            <span class="fa fa-check text-green-500 font-bold"></span> Member, Ministerial Committee on COVID 19 Herbal Remedies and Natural Compounds by the Minister, Federal Ministry of Science & Technology, June 2020  <br />
-          </p>
-  
-       
-  
-         
-        </div>
+
+      <div class="mt-5 md:mt-0 hidden md:grid">
+        <img src="/img/img.jpg" class="rounded-xl" />
       </div>
     </div>
-  </template>
+
+    <div class="grid md:grid-cols-2 place-items-start md:space-x-10 md:P-24 mt-24 pb-10">
+      <div class="mb-5 md:mt-0">
+        <img src="/img/img2.jpg" class="rounded-xl" />
+      </div>
+      <div class="grid grid-cols-1 space-y-5">
+        <p class="font-bold text-blue-800 text-xl">What We Do</p>
+        <p class="text-4xl font-bold text-semibold">Our Background</p>
+        <p class="  ">
+          Prof. Bertram Ekejiuba Bright Nwoke is from Umuariam, Obowo LGA in Imo
+          State, Nigeria. He was a pioneer student of Institute of Management
+          and Technology, Enugu where he obtained OND (Science Laboratory
+          Technology) in 1975. He was one of the pioneer students of University
+          of Jos who matriculated in 1976. He obtained B.Sc. (Zoology) Second
+          Class Upper Division in 1979 and came out as the first best graduating
+          student of Zoology, University of Jos. He also has to his credit an
+          MSc degree in Applied Entomology & Parasitology (1981) and PhD degree
+          in Medical Parasitology & Entomology (1987) all from the University of
+          Jos. He is well trained in Total Quality Management and has a
+          certificate on it.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="gid grid-cols-1 place-items-center" >
+  <p class="mt-10 text-4xl text-center font-bold   text-2xl text-[#3f3a64] ">More Services</p>
+</div>
+
+  <div
+    class="grid md:grid-cols-2 md:pl-40 md:pr-40 pr-5 pl-5  mb-10 pt-10 space-x-10 place-items-start"
+  >
+    <div class="grid space-y-5">
+      <p class="text-center md:text-start">CONTRIBUTIONS TO THE GROWTH AND DEVELOPMENT OF SCIENCE</p>
+      <p class="text-2xl font-bold text-semibold text-center">SERVICES TO SCIENCE</p>
+      <p>
+        Prof. B. E. B. Nwoke has strongly contributed to academic leadership and growth as well as stimulated superior and excellent research studies and learning in the University. He has been involved in external examination of postgraduate students (PhD and or Masters Degree) in 12 Universities and in the assessment of more than 50 colleagues to the ranks of Professor/Reader in Nigeria and abroad. He has also successfully supervised more than 300 undergraduate projects, at least 67 Masters Degree and at least 75 PhD Theses. Out these postgraduates, 22 of them are Professors. One of his postgraduate students is now a Vice Chancellor and another one is currently a Deputy Vice Chancellor. He has to his credit not less than 213 academic and professional publications (journal, proceedings, technical reports, book, and book chapter). Prof Nwoke has been involved in the postgraduate external examination of the following Institutions
+      </p>
+
+      <div class="space-y-5">
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Federal University of Technology Owerri, (MSc & PhD)<br />
+        </p>
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> University of Nigeria, Nsukka (MSc & PhD)<br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> University of Benin (Doctor of Optometry, OD)
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> University of Jos (PhD)<br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> University of Calabar (PhD)
+          <br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> University of Port Harcourt (PhD)<br />
+        </p>
+
+        <!-- ==== -->
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> University of Abuja (PhD)<br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Nnamdi Azikiwe University, Awka (MSc & PhD)
+        </p>
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Ambrose Ali University, Ekpoma (PhD)<br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Delta State University, Abraka (MSc)<br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Michael Okpara University of Agriculture, Umuahia (MSc & PhD)<br />
+        </p>
+      </div>
+    </div>
+
+    <div>
+      <div class="md:space-y-5 space-y-2 mt-5 md:mt-0">
+        <p class="text-center">SERVICE TO HUMANITY</p>
+        <p class="text-sm md:text-2xl font-bold text-semibold text-center">CONTRIBUTIONS TO GOVERNMENT AND COMMUNITY SERVICE</p>
+
+       
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> 	Pioneer Board Member, Imo State University Teaching Hospital Board, 2003-2007<br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Chairman, Imo State Technical Committee on the Review of Curriculum and Restructuring of Michael Okpara College of Agriculture [MOCA] Umuagwo, 2004. This led to the establishment of Michael Okpara Polytechnic Umuagwo
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Chairman, Imo State Government Board of Inquiry into the Health Management Board, 2005 <br />
+        </p>
+
+
+
+        <!-- ================================== -->
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Member, Imo State Government 2007 Transition Committee<br />
+        </p>
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Chairman, Sub-Committee on Land and Environment, Imo State Government 2007 Transition Committee<br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span>  Chairman, Imo State Government Board of Inquiry into the Health Management Board, 2005  
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span>  Chairman, Sub-Committee on Land and Environment, Imo State Government 2007 Transition Committee<br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> 	Chairman, Ministerial Committee of the Review of the Industrial Training Fund (ITF) System Audit, June-July, 2008.
+          <br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Steering Committee Member, Imo State Vision 20: 2020/>
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Chairman, Imo State University Vision 20:2020<br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span>  Chairman, Imo State Government Special Committee on the Return of Secondary Schools to Voluntary Agencies and Missionaries, 2009.<br />
+        </p>
+
+        <p>
+          <span class="fa fa-check text-green-500 font-bold"></span> Member, Ministerial Committee on COVID 19 Herbal Remedies and Natural Compounds by the Minister, Federal Ministry of Science & Technology, June 2020  <br />
+        </p>
+
+     
+
+       
+      </div>
+    </div>
+  </div>
+</template>
