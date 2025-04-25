f22841821e7dbf8127026405e1919b9202e62a29
danielnifty1 <90337586+danielnifty1@users.noreply.github.com>
2025-04-25 15:29:24 +0100

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,8 +9,8 @@ const count = ref(0)
 <template>
   <h1 class="bg-red-900">{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+  <div class="card flex justify-center items-center">
+    <button type="button" @click="count++" class="bg-red-900 ring-[1.5px] text-white ring-md " > click here to count {{ count }}</button>
     
   </div>
 
